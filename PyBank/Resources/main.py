72b0cf60c3f620a67fc42af5a414f4947a70bab8
mariav0721 <mane0721@gmail.com>
2023-07-01 12:05:58 -0700

    Almost_there

--- PyBank/Resources/main.py
+++ PyBank/Resources/main.py
@@ -17,7 +17,9 @@ with open(csvpath, 'r') as csvfile:
     total_profit_losses = 0
     last_losses = 0
     change = 0
-
+    sum_profit_losses =0
+    greatest_increase =0
+    greatest_decrease=0
 #Start the Loop
 
     for row in csvreader:
@@ -28,10 +30,29 @@ with open(csvpath, 'r') as csvfile:
         total_months= total_months+1
     
         total_profit_losses = total_profit_losses+ profit_losses
-        
-        change()
+
+        if total_months> 1:
+            change= profit_losses-last_losses
+
+            sum_profit_losses = sum_profit_losses + change
+        last_losses=int(row[1])
+
+
+        average_change = sum_profit_losses/total_months-1
+
+    # Find greatest changes 
     
+        if change > greatest_increase:
+            greatest_increase = change
+            greatest_increase_date = date
+    
+        if change < greatest_decrease:
+            greatest_decrease = change
+            greatest_decrease_date = date
+
+
     # After the loop, print the summary
+
     print('Module 3 challenge')
     print('Maria Valdivieso')
 
@@ -40,6 +61,9 @@ with open(csvpath, 'r') as csvfile:
     print('Financial Analysis Summary')
     print('------------------------------------------------------------')
     print(f'Total Months: {total_months}')
-    print(f'Total Profit/Losses: {total_profit_losses}')
+    print(f'Total Profit/Losses: $ {total_profit_losses}')
+    print(f'Average Change: {average_change}')
+    print(f'Greatest Increase in Profits: {greatest_increase_date} (${greatest_increase})')
+    print(f'Greatest Decrease in Profits: {greatest_decrease_date} (${greatest_decrease})')
 
-
+ 
