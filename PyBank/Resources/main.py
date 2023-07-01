7d7174fec400d159ef7ec819d80379cb9c8d5a0e
mariav0721 <mane0721@gmail.com>
2023-07-01 10:50:11 -0700

    type some message

--- PyBank/Resources/main.py
+++ PyBank/Resources/main.py
@@ -1,2 +1,17 @@
+# Look for the data file 
+import csv
 
-#Hellogit 
+csvpath = "C:/Users/manen/Desktop/class/ASU/Homework/python-challenge/PyBank/Resources/budget_data.csv"
+
+with open(csvpath, 'r') as csvfile:
+    csvreader = csv.reader(csvfile)
+    
+    # Skip the header row if it exists
+    csv_header = next(csvreader)
+
+    # Read and process each row of the CSV file
+    for row in csvreader:
+        # Access the data in each row
+        date = row[0]
+        # Testing conection
+        print(date)
