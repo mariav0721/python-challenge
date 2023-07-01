2d6c0e0acc0707970e37552669afa8d8b7aa75f8
mariav0721 <mane0721@gmail.com>
2023-07-01 11:36:09 -0700

    Second_try

--- PyBank/Resources/main.py
+++ PyBank/Resources/main.py
@@ -1,17 +1,45 @@
 # Look for the data file 
 import csv
+import io
 
 csvpath = "C:/Users/manen/Desktop/class/ASU/Homework/python-challenge/PyBank/Resources/budget_data.csv"
 
 with open(csvpath, 'r') as csvfile:
     csvreader = csv.reader(csvfile)
     
-    # Skip the header row if it exists
+    
     csv_header = next(csvreader)
+   
 
-    # Read and process each row of the CSV file
+#Set variables
+
+    total_months = 0
+    total_profit_losses = 0
+    last_losses = 0
+    change = 0
+
+#Start the Loop
+
     for row in csvreader:
-        # Access the data in each row
-        date = row[0]
-        # Testing conection
-        print(date)
+        
+        date= row[0]
+        profit_losses = int(row[1])
+
+        total_months= total_months+1
+    
+        total_profit_losses = total_profit_losses+ profit_losses
+        
+        change()
+    
+    # After the loop, print the summary
+    print('Module 3 challenge')
+    print('Maria Valdivieso')
+
+    print("                                  ")
+
+    print('Financial Analysis Summary')
+    print('------------------------------------------------------------')
+    print(f'Total Months: {total_months}')
+    print(f'Total Profit/Losses: {total_profit_losses}')
+
+
