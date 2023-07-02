b6a2f0c6eba30d5f044804b19479b901e80954ba
mariav0721 <mane0721@gmail.com>
2023-07-02 00:00:52 -0700

    Almost_done

--- PyPoll/Resources/main.py
+++ PyPoll/Resources/main.py
@@ -1,4 +1,5 @@
 #look fro the data file
+from ast import If
 import csv
 
 csvpath = "C:/Users/manen/Desktop/class/ASU/Homework/python-challenge/PyPoll/Resources/election_data.csv"
@@ -13,6 +14,9 @@ with open(csvpath, 'r') as csvfile:
     diana_votes = 0
     raymon_votes = 0
     total_votes = 0
+    charles_percentage =0
+    diana_percentage=0
+    raymon_percentage=0
 
     # Start the loop
     for row in csvreader:
@@ -24,6 +28,28 @@ with open(csvpath, 'r') as csvfile:
             raymon_votes = raymon_votes+1  
     
 
+#total votes
+
+total_votes = charles_votes +diana_votes+ raymon_votes
+
+
+charles_percentage=(charles_votes/total_votes)*100
+diana_percentage= (diana_votes/ total_votes)*100
+raymon_percentage= (raymon_votes/total_votes)*100
+
+#Select winner
+winner=""
+
+if (charles_percentage > diana_percentage) and (charles_percentage > raymon_percentage):
+    winner = "Charles Casper Stockham"
+elif (diana_percentage > charles_percentage) and (diana_percentage > raymon_percentage):
+    winner = "Diana DeGette"
+elif (raymon_percentage > charles_percentage) and (raymon_percentage > diana_percentage):
+    winner = "Raymon Anthony Doane"
+
+
+
+
 # After the loop, print the summary
 
 print('Module 3 challenge')
@@ -33,17 +59,18 @@ print("                                  ")
 
 print('Elections Results')
 print('------------------------------------------------------------')
-#print(f'Total Votes: {total_votes}')
+
+print(f'Total Votes: {total_votes}')
 
 print('------------------------------------------------------------')
 
-print(charles_votes)
-print(diana_votes)
-print(raymon_votes)
-#print(f'Charles: {charles_percentage:}% ({charles_votes})')
-#print(f'Diana: {diana_percentage:}% ({diana_votes})')
-#print(f'Raymon: {raymon_percentage:}% ({raymon_votes})')
-#print('------------------------------------------------------------')
+
+print(f'Charles: {charles_percentage: .2f}% ({charles_votes})')
+print(f'Diana: {diana_percentage:.2f}% ({diana_votes})')
+print(f'Raymon: {raymon_percentage:.2f}% ({raymon_votes})')
+print('------------------------------------------------------------')
+
+print(f'Winner:{winner}')
 
 
  #Write txt file
